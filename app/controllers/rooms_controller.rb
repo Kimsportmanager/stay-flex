cc0153c69590fcaf3b1c59160c3a3162c1769f81
Guillaume Roux <guillaume500@hotmail.fr>
2023-06-12 16:39:14 +0200

    added gems(geocoder/pg search) and a search bar on home page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,57 +1,123 @@
 class RoomsController < ApplicationController
-  # GET /rooms
+
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_boat, only: [:show, :edit, :update, :destroy]
+
   def index
-     @rooms = Room.all
-  end
 
-  # GET /rooms/new
-  def new
-    @room = Room.new
-  end
-
-  # POST /rooms
-  def create
-    @room = Room.new(room_params)
-
-    if @room.save
-      redirect_to @room, notice: 'Room was successfully created.'
+    if params[:query].present?
+      @rooms = policy_scope(Room).search_by_title_and_address(params[:query])
     else
-      render :new
+      @rooms = policy_scope(Room)
+      authorize @rooms
+    end
+    @markers = @rooms.where.not(latitude: nil, longitude: nil).map do |boat|
+      {
+        lat: boat.latitude,
+        lng: boat.longitude
+      }
     end
   end
 
-  # GET /rooms/:id
   def show
-    @room = Room.find(params[:id])
+    @review = Review.new
+    @booking = Reservation.new
   end
 
-  # GET /rooms/:id/edit
+  def new
+    @room = Room.new
+    authorize @room
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user = current_user
+    authorize @room
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+
+  end
+
   def edit
-    @room = Room.find(params[:id])
   end
 
-  # PATCH/PUT /rooms/:id
   def update
-    @room = Room.find(params[:id])
-
     if @room.update(room_params)
-      redirect_to @room, notice: 'Room was successfully updated.'
+      redirect_to boat_path(@room)
     else
       render :edit
     end
   end
 
-  # DELETE /rooms/:id
   def destroy
-    @room = Room.find(params[:id])
-    @room.destroy
-
-    redirect_to rooms_url, notice: 'Room was successfully destroyed.'
+    if @room.destroy
+      redirect_to rooms_path(@rooms)
+    else
+      render :index
+    end
   end
 
   private
 
+  def set_room
+    @room = Room.find(params[:id])
+    authorize @room
+  end
+
   def room_params
-    params.require(:room).permit(:title, :description, :price)
+    params[:room].permit(:users_id, :city, :neighborhood, :room_number, :bathroom_number, :bed_number, :max_ccupants, :price, :room_pictures, :available_start_date, :available_end_date, :title, :description)
   end
 end
+
+
+
+
+  ## GET /rooms
+  ##def index
+    ## @rooms = Room.all
+  ##end
+
+  # GET /rooms/new
+  ##def new
+    ##@room = Room.new
+  ##end
+
+  # POST /rooms
+  ##def create
+   ## @room = Room.new(room_params)
+
+   ## if @room.save
+     ## redirect_to @room, notice: 'Room was successfully created.'
+   ## else
+     ## render :new
+   ## end
+  ##end
+
+  # GET /rooms/:id
+  ##def show
+  #### @room = Room.find(params[:id])
+  ##end
+
+  # PATCH/PUT /rooms/:id
+  ##def update
+  ##  @room = Room.find(params[:id])
+
+    ##if @room.update(room_params)
+     ## redirect_to @room, notice: 'Room was successfully updated.'
+   ## else
+    ##  render :edit
+    ##end
+  ##end
+
+  # DELETE /rooms/:id
+  ##def destroy
+   ## @room = Room.find(params[:id])
+   ## @room.destroy
+
+   ## redirect_to rooms_url, notice: 'Room was successfully destroyed.'
+  ##end
+
+  ####end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_173406) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_143632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_173406) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["users_id"], name: "index_rooms_on_users_id"
   end
 
